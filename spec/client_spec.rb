4c3a7c555625ab33177c7b8aadfeb85b3631ce13
Jon Ho <lj080805@gmail.com>
2015-12-05 04:11:09 -0800

    more tests!

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,20 +2,23 @@ require 'spec_helper'
 
 describe Lolxin::Client do
   let(:client) { client = Lolxin::Client.new ENV['API_KEY'] }
+  let(:regions) { regions = Lolxin::Region::REGIONS }
 
-  context '#new' do
-    it 'should set region to na if options hash missing region key' do
-      expect(client.region).to eq('na')
+  describe '#new' do
+    context 'no region supplied' do
+      it 'should set region to na' do
+        expect(client.region).to eq('na')
+      end
     end
 
-    it 'should set region to kr with options hash' do
-      @client = Lolxin::Client.new ENV['API_KEY'], :region => 'kr'
-      expect(@client.region).to eq('kr')
-    end
-
-    it 'should set region to br with options hash' do
-      @client = Lolxin::Client.new ENV['API_KEY'], :region => 'br'
-      expect(@client.region).to eq('br')
+    context 'region supplied' do
+      # couldn't use regions here because can only use let variables inside it-scopes
+      Lolxin::Region::REGIONS.each do |key, value|
+        it "should set region to #{value}" do
+          @client = Lolxin::Client.new ENV['API_KEY'], :region => value
+          expect(regions[@client.region.upcase.to_sym]).not_to be_nil
+        end
+      end
     end
   end
 end
