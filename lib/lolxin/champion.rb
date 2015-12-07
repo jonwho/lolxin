b08d90df2d6342afedc5fb504bce590923dcd29c
Jon Ho <lj080805@gmail.com>
2015-12-06 21:25:08 -0800

    champion dynamic attr_reader

--- lib/lolxin/champion.rb
+++ lib/lolxin/champion.rb
@@ -1,20 +1,13 @@
 module Lolxin
   class Champion
-    attr_reader :id,
-                :active,
-                :botEnabled,
-                :freeToPlay,
-                :botMmEnabled,
-                :rankedPlayEnabled
-
     # Idea now is to build a Champion with as much data as possible.
-    def initialize(options = {})
-      @id = options[:id]
-      @active = options[:active]
-      @botEnabled = options[:botEnabled]
-      @freeToPlay = options[:freeToPlay]
-      @botMmEnabled = options[:botMmEnabled]
-      @rankedPlayEnabled = options[:rankedPlayEnabled]
+    def initialize(options)
+      raise "Expecting Hash argument for Champion constructor" unless options.instance_of? Hash
+      raise "Cannot provide an emtpy Hash for Champion constructor" if options.empty?
+      options.each do |key, value|
+        self.class.send(:attr_reader, key)
+        self.instance_variable_set("@#{key}", value)
+      end
     end
 
   end
