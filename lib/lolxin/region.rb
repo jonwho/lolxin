77d3154d6344646d311e22bf0156c4b3c91862de
Jon Ho <lj080805@gmail.com>
2016-12-27 21:57:27 -0800

    drop hash rocket syntax

--- lib/lolxin/region.rb
+++ lib/lolxin/region.rb
@@ -1,19 +1,17 @@
 module Lolxin
   module Region
-
     REGIONS = {
-      :BR   => 'br',
-      :EUNE => 'eune',
-      :EUW  => 'euw',
-      :KR   => 'kr',
-      :LAN  => 'lan',
-      :LAS  => 'las',
-      :NA   => 'na',
-      :OCE  => 'oce',
-      :PBE  => 'pbe',
-      :RU   => 'ru',
-      :TR   => 'tr'
+      BR:   'br',
+      EUNE: 'eune',
+      EUW:  'euw',
+      KR:   'kr',
+      LAN:  'lan',
+      LAS:  'las',
+      NA:   'na',
+      OCE:  'oce',
+      PBE:  'pbe',
+      RU:   'ru',
+      TR:   'tr'
     }
-
   end
 end
